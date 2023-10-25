fa95d07bb32bf4859526aec33b06a347d2ac1baa
ImDreezycz <83569916+Renziq@users.noreply.github.com>
2023-10-26 01:13:17 +0200

    Add files via upload

---
+++ drz_boatrental-main/config.lua
@@ -0,0 +1,14 @@
+DRZ = {}
+
+DRZ.Loc = {
+    
+    BlipLocation = {
+        {x = -1600.08, y = -1176.98, z = 1.51},
+    }
+}
+DRZ.PedLocation = vector4(-1600.08, -1176.98, 0.51, 300.47) --TARGET
+DRZ.Menu = vector4(-1600.08, -1176.98, 1.51, 0.88) --TARGET
+DRZ.SpawnBoat = vector4(-1630.47, -1191.95, 0.12, 104.88) --SPAWN BOAT
+
+DRZ.Seashark = "seashark" --SEASHARK MODEL
+DRZ.Boat = "dinghy2" --BOAT MODEL
